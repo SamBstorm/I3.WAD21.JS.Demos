82832529f535e6a15303968a582156027c0916d6
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-09 16:53:06 +0200

    Upadte Demo-11

--- Demo-11/scripts/main.js
+++ Demo-11/scripts/main.js
@@ -1,15 +1,22 @@
-function AddNewElement(listId, nameItem = 'New Item'){
+function AddNewElement(listId, nameItem = 'New Item') {
     let listElement = document.getElementById(listId);
-    if(listElement == undefined) throw new TypeError(`L'objet avec l'id ${listId} n'existe pas...`);
-    if(listElement.nodeName != "UL") throw new TypeError(`L'objet avec l'id ${listId} n'est pas une liste...`);
+    if (listElement == undefined) throw new TypeError(`L'objet avec l'id ${listId} n'existe pas...`);
+    if (listElement.nodeName != "UL") throw new TypeError(`L'objet avec l'id ${listId} n'est pas une liste...`);
 
     let new_liElement = document.createElement('li');
     listElement.appendChild(new_liElement);
     new_liElement.innerText = nameItem;
+    changeTitle();
 }
 
-let student_names = ['Judy','Valérie','Anaïs', 'Assmaa', 'Laure','Caroline', 'Marwa','Julie','Ylenia','Grace','Minh','Stefania', 'Nicole','Jennifer','Myriam' ];
+let student_names = ['Judy', 'Valérie', 'Anaïs', 'Assmaa', 'Laure', 'Caroline', 'Marwa', 'Julie', 'Ylenia', 'Grace', 'Minh', 'Stefania', 'Nicole', 'Jennifer', 'Myriam'];
 
 for (const student_name of student_names) {
     AddNewElement('ul_1', student_name);
 }
+
+function changeTitle() {
+    let title = document.getElementById("ul_1").parentNode.firstElementChild;
+    title.innerText = 'Liste 1 : ' + document.getElementById('ul_1').childNodes.length;
+}
+changeTitle();
